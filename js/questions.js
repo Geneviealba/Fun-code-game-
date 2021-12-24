3e5419b159f95193804ef259bb407af6e295abec
Genevie Giko <gikogenevie@yahoo.com>
2021-12-24 13:12:16 -0500

    Game play

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question:"When a user views a page containing a JavaScript program, which machine actually executes the script?",
+    answer: "The web server",
+    options: [
+      "a central machine deep within netscape corporate offices",
+      "The web server",
+      "A central machine deep within netscape corporate offices",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Why so JavaScript and Java have similar name?",
+    answer: "JavaScript's syntax is loosely based on Java's",
+    options: [
+      "JavaScript is a stripped-down version of Java",
+      "They both originated on the island of Java",
+      " Varying randomly",
+      "JavaScript's syntax is loosely based on Java's"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following can't be done with client-side JavaScript?",
+    answer: "Storing the form's contents to a database file on the server",
+    options: [
+      "Storing the form's contents to a database file on the server",
+      "Validating a form",
+      "Sending a form's contents by email",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following are capabilities of functions in JavaScript?",
+    answer: "Accept parameters",
+    options: [
+      "Return a value",
+      "Accept parameters and Return a value",
+      "Statement Question Language",
+      "Accept parameters"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following navigator object properties is the same in both   Netscape and IE?",
+    answer: "navigator.appCodeName",
+    options: [
+      "navigator.appCodeName",
+      "navigator.appName",
+      "navigator.appVersion",
+      "None of the above"
+    ]
+  },
+
+];
